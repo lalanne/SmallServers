45faa8b207dfb6b13a013742c878364da902fc31
Christian Lalanne <christian.lalanne@zweicom.com>
2017-06-02 11:09:56 -0400

    ADD: live server

--- client.py
+++ client.py
@@ -17,4 +17,20 @@ remote_port = 4040
 s.connect((remote_ip, remote_port))
 print 'Socket connected to ' + remote_ip + ' on ip ' + remote_ip
 
+message = '093029i0klfjwlkfjlwkjkjqfoijwoefijoiejwf'
+
+try :
+    s.sendall(message)
+except socket.error:
+    print 'Send failed'
+    sys.exit()
+ 
+print 'Message send successfully'
+
+#Now receive data
+reply = s.recv(4096)
+ 
+print 'message from server[' + reply + ']'
+
+s.close()
 
--- server.py
+++ server.py
@@ -4,4 +4,42 @@
 #Accept connections
 #Read(receive) and response(send)
 
+import socket
+import sys
+
+IP = '127.0.0.1'
+PORT = 4040
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print 'Socket created'
+
+try:
+    s.bind((IP, PORT))
+except socket.error, msg:
+    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    sys.exit()
+
+print 'Socket bind complete'
+
+s.listen(10) #backlog = 10, if incoming an 11th connection the request should be rejected
+print 'Now socket is listening'
+
+while 1:
+    conn, addr = s.accept()  #blocking call
+    print 'Connected with ' + addr[0] + ':' + str(addr[1])
+
+    data = conn.recv(1024)
+
+    print 'receiving: [' + data + ']'
+
+    data += '[from the server]'
+
+    if not data: 
+        break
+
+    conn.sendall(data)
+     
+conn.close()
+s.close()
+
 
