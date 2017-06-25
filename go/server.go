fcab44c02136a0e20c3f7b7b119e04495d965434
orc <lalanne@gmail.com>
2017-06-25 19:47:27 -0400

    ADD: unithread server in go

---
+++ go/server.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+const (
+	CONN_HOST = "localhost"
+	CONN_PORT = "4040"
+	CONN_TYPE = "tcp"
+)
+
+func main() {
+    fmt.Println("server initiating.....!")
+
+	// Listen for incoming connections.
+    l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
+
+	if err != nil {
+        fmt.Println("Error listening:", err.Error())
+        os.Exit(1)
+    }
+
+	// Close the listener when the application closes.
+	defer l.Close()
+
+	fmt.Println("Listening on " + CONN_HOST + ":" + CONN_PORT)
+
+	for {
+		// Listen for incoming connection
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting: ", err.Error())
+            os.Exit(1)
+		}
+
+		//for multithreaded server
+		//go handleRequest(conn)
+		handleRequest(conn)
+	}
+}
+
+func handleRequest(conn net.Conn) {
+	// Make a buffer to hold incoming data.
+	buf := make([]byte, 1024)
+	// Read the incoming connection into the buffer.
+	reqLen, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("Error reading:", err.Error())
+	}
+	fmt.Println("incoming len:", reqLen)
+	// Send a response back to person contacting us.
+	conn.Write([]byte("Message received."))
+	// Close the connection when you're done with it.
+	conn.Close()
+}
+
+
+
