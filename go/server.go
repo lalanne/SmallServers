a460c71268cb1f542d2ce17a1ac87105d307fbb8
Christian Lalanne <christian.lalanne@zweicom.com>
2017-06-27 09:53:42 -0400

    ADD: partially functioning go server

--- go/server.go
+++ go/server.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	//"code.google.com/p/go-charset/charset"
+	"bytes"
+	"encoding/xml"
 	"fmt"
+	"golang.org/x/net/html/charset"
 	"net"
 	"os"
 )
@@ -13,15 +17,15 @@ const (
 )
 
 func main() {
-    fmt.Println("server initiating.....!")
+	fmt.Println("server initiating.....!")
 
 	// Listen for incoming connections.
-    l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
+	l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
 
 	if err != nil {
-        fmt.Println("Error listening:", err.Error())
-        os.Exit(1)
-    }
+		fmt.Println("Error listening:", err.Error())
+		os.Exit(1)
+	}
 
 	// Close the listener when the application closes.
 	defer l.Close()
@@ -33,13 +37,27 @@ func main() {
 		conn, err := l.Accept()
 		if err != nil {
 			fmt.Println("Error accepting: ", err.Error())
-            os.Exit(1)
+			os.Exit(1)
 		}
 
 		//for multithreaded server
 		//go handleRequest(conn)
 		handleRequest(conn)
 	}
+}
+
+type Req struct {
+	Op      string `xml:"op"`
+	Msisdn  string `xml:"msisdn"`
+	IdPromo string `xml:"idPromo"`
+}
+
+type Head struct {
+	XMLName xml.Name `xml:"header"`
+}
+
+type Msg struct {
+	Request Req `xml:"req"`
 }
 
 func handleRequest(conn net.Conn) {
@@ -51,12 +69,18 @@ func handleRequest(conn net.Conn) {
 		fmt.Println("Error reading:", err.Error())
 	}
 	fmt.Println("incoming len:", reqLen)
-    fmt.Printf("incoming buffer: %s", buf)
+	fmt.Printf("incoming buffer: %s", buf)
+
+	var message Msg
+	//xml.Unmarshal(buf, &message)
+	reader := bytes.NewReader(buf)
+	decoder := xml.NewDecoder(reader)
+	decoder.CharsetReader = charset.NewReaderLabel
+	err = decoder.Decode(&message)
+	fmt.Println("msisdn:[", message.Request.Msisdn)
+
 	// Send a response back to person contacting us.
 	conn.Write([]byte("Message received."))
 	// Close the connection when you're done with it.
 	conn.Close()
 }
-
-
-
