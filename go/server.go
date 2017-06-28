ce6b9e41c05180dbcbdb61796b65730d6f0401c8
Christian Lalanne <christian.lalanne@zweicom.com>
2017-06-28 00:51:59 -0400

    DEL: comments

--- go/server.go
+++ go/server.go
@@ -16,9 +16,6 @@ const (
 )
 
 func main() {
-	fmt.Println("server initiating.....!")
-
-	// Listen for incoming connections.
 	l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
 
 	if err != nil {
@@ -26,21 +23,17 @@ func main() {
 		os.Exit(1)
 	}
 
-	// Close the listener when the application closes.
 	defer l.Close()
 
 	fmt.Println("Listening on " + CONN_HOST + ":" + CONN_PORT)
 
 	for {
-		// Listen for incoming connection
 		conn, err := l.Accept()
 		if err != nil {
 			fmt.Println("Error accepting: ", err.Error())
 			os.Exit(1)
 		}
 
-		//for multithreaded server
-		//go handleRequest(conn)
 		handleRequest(conn)
 	}
 }
@@ -138,9 +131,7 @@ func buildResponse(msisdn string) string {
 }
 
 func handleRequest(conn net.Conn) {
-	// Make a buffer to hold incoming data.
 	buf := make([]byte, 1024)
-	// Read the incoming connection into the buffer.
 	reqLen, err := conn.Read(buf)
 	if err != nil || reqLen <= 0 {
 		fmt.Println("Error reading:", err.Error())
@@ -158,11 +149,8 @@ func handleRequest(conn net.Conn) {
 
 	var payload string = buildResponse(message.Request.Msisdn)
 
-	// Send a response back to person contacting us.
-	//conn.Write([]byte("Message received."))
 	if payload != "no response" {
 		conn.Write([]byte(payload))
-		// Close the connection when you're done with it.
 		conn.Close()
 	}
 }
