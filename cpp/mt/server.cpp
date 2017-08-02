ffe47e028efe54e83c9e3998bce7fec1dc5cd2ab
Christian Lalanne <christian.lalanne@zweicom.com>
2017-08-01 22:25:55 -0400

    #4 CHG: extracting functions into their own files

--- cpp/mt/server.cpp
+++ cpp/mt/server.cpp
@@ -1,17 +1,14 @@
 
+#include "response.hpp"
+
 #include <boost/array.hpp>
-#include <boost/property_tree/xml_parser.hpp>
-#include <boost/property_tree/ptree.hpp>
-#include <boost/foreach.hpp>
 #include <boost/asio.hpp>
 
 #include <future>
 #include <iostream>
 #include <string>
-#include <sstream>
 
 using boost::asio::ip::tcp;
-using boost::property_tree::ptree;
 
 using namespace std;
 
@@ -19,106 +16,6 @@ const int PORT                          = 4040;
 const int BUFF_LENGTH                   = 256;
 const int SUCCESS                       = 0;
 
-const string RSLT_EXITO                    = "0";
-const string RSLT_ERR_PROMO_INEXISTENTE    = "4";
-const string RSLT_ERR_SIN_SALDO_PP         = "14";
-const string RSLT_ERR_TIPO_CLIENTE_DST     = "18";
-
-string extract_msisdn(const string& message) {
-    istringstream is(message);
-    string msisdn;
-    ptree pt;
-
-    read_xml(is, pt);
-    BOOST_FOREACH( ptree::value_type const& v, pt.get_child("msg") ) {
-         if( v.first == "req" ) {
-            msisdn = v.second.get<string>("msisdn");
-         }
-    }
-
-    //cout << "[extract_msisdn] msisdn[" << msisdn  << "]" << endl;
-    return msisdn;
-}
-
-string build_response(const string& msisdn, const string& result) {
-	stringstream response;
-    response << "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n <msg>\n\t" <<
-            "<header action=\"1\" id=\"1111\"  />\n\t" <<
-            "<resp>\n\t\t" << 
-            "<op>rslt_comp_promo</op>\n\t\t" << 
-            "<msisdn>" + msisdn + "</msisdn>\n\t\t" <<
-            "<result>" + result + "</result>\n\t\t" <<
-            "<strresult>OK</strresult>\n\t" <<
-            "</resp>\n" <<
-            "</msg>\n";
-
-    return response.str();
-}
-
-string select_message(string& msisdn) {
-    string response = "no response";
-
-    if(msisdn == "56999694443") {
-        response = build_response(msisdn, RSLT_ERR_PROMO_INEXISTENTE);
-    }
-    else if(msisdn == "56999694444") {
-        response = build_response(msisdn, RSLT_EXITO);
-    }
-    else if(msisdn == "56999694448") {
-        response = build_response(msisdn, RSLT_EXITO);
-    }
-    else if(msisdn == "56999694449") {
-        response = build_response(msisdn, RSLT_EXITO);
-    }
-    else if(msisdn == "56999694450") {
-        response = build_response(msisdn, RSLT_EXITO);
-    }
-    else if(msisdn == "56999694451") {
-        response = build_response(msisdn, RSLT_EXITO);
-    }
-    else if(msisdn == "56999694452") {
-        response = build_response(msisdn, RSLT_EXITO);
-    }
-    else if(msisdn == "56999694453") {
-        response = build_response(msisdn, RSLT_EXITO);
-    }
-    else if(msisdn == "56999694454") {
-        response = build_response(msisdn, RSLT_EXITO);
-    }
-    else if(msisdn == "56999694455") {
-        response = build_response(msisdn, RSLT_EXITO);
-    }
-    else if(msisdn == "56999694456") {
-        response = build_response(msisdn, RSLT_EXITO);
-    }
-    else if(msisdn == "56999694457") {
-        response = build_response(msisdn, RSLT_EXITO);
-    }
-    else if(msisdn == "56999694458") {
-        response = build_response(msisdn, RSLT_EXITO);
-    }
-    else if(msisdn == "56999694459") {
-        response = build_response(msisdn, RSLT_EXITO);
-    }
-    else {
-        const string epilogue = msisdn.substr(msisdn.length() - 2);
-
-        if(epilogue == "14") {
-            response = build_response(msisdn, RSLT_ERR_SIN_SALDO_PP);
-        }
-        else if(epilogue == "10") {
-            response = build_response(msisdn, RSLT_ERR_TIPO_CLIENTE_DST);
-        }
-        else if(epilogue == "25") {
-            response = "no response";
-        }
-        else {
-            cout << "[ERROR] recognized msisdn NOT [" <<  msisdn << "]" << endl;
-        }
-    }
-
-    return response;
-}
 
 void handle_connection(tcp::socket& socket, 
                     boost::array<char, BUFF_LENGTH>& buf,
