9a14b14cd2d119055e1af4bfd1d116a60dfa69db
orc <lalanne@gmail.com>
2017-08-08 08:47:29 -0400

    #13 ADD: build script until now for go folder

--- build.sh
+++ build.sh
@@ -35,3 +35,41 @@ fi
 cd -
 echo ""
 
+echo ""
+echo "Building GO..."
+cd go/
+go clean
+go install common
+go install st
+go install mt
+cd -
+echo ""
+
+echo ""
+echo "Building GO unit tests..."
+echo ""
+
+echo ""
+echo "Executing GO unit tests..."
+path=`pwd`
+export GOPATH=$path/go/
+go test common -cover
+ret=$?
+if [ "$ret" = "0" ]; then
+    echo "GO UTESTS SUCCESS!!!!"
+else
+    echo "GO UTESTS FAILED!!!!!"
+    exit 1 
+fi 
+cd -
+echo ""
+
+
+
+
+
+
+
+
+
+
