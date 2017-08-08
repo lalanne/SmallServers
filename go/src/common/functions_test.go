b7953ae7f0f199bb1ae7102370e9c7d1b312afef
orc <lalanne@gmail.com>
2017-08-07 23:13:33 -0400

    #9 CHG: renaming

--- go/src/common/functions_test.go
+++ go/src/common/functions_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func buildMessage(msisdn string) string {
+func buildRequestMessage(msisdn string) string {
 	var message string = "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>" +
 		"<msg>" +
 		"<header action=\"1\" id=\"1111\"  />" +
@@ -22,7 +22,7 @@ func buildMessage(msisdn string) string {
 func TestParseMsisdn(t *testing.T) {
 	const expected_msisdn = "56999694444"
 	buf := make([]byte, 1024)
-	buf = []byte(buildMessage(expected_msisdn))
+	buf = []byte(buildRequestMessage(expected_msisdn))
 	var actual_msisdn string = ParseMsisdn(buf)
 
 	if expected_msisdn != actual_msisdn {
