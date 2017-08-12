15797e7dafefff64c88f04f45bcddd5bd1577c8f
orc <lalanne@gmail.com>
2017-08-12 01:17:50 -0400

    #31 ADD: execution of functional tests cpp from build scripts

--- build_cpp.sh
+++ build_cpp.sh
@@ -1,25 +1,78 @@
 #!/usr/bin/env bash
 
-echo ""
-echo "Building CPP..."
-echo ""
-cd cpp
-cmake .
-make clean
-make
-cd -
-echo ""
 
-echo "Executing CPP unit tests..."
-cd cpp/common/utst/
-echo `pwd`
-./tests
+build_cpp() {
+    echo ""
+    echo "Building CPP..."
+    echo ""
+    cd cpp
+    cmake .
+    make clean
+    make
+    cd -
+    echo ""
+}
+
+execute_unit_tests_cpp() {
+    echo "Executing CPP unit tests..."
+    cd cpp/common/utst/
+    echo `pwd`
+    ./tests
+    ret=$?
+    if [ "$ret" = "0" ]; then
+        echo "CPP UTESTS SUCCESS!!!!"
+    else
+        echo "CPP UTESTS FAILED!!!!!"
+        exit 1 
+    fi 
+    cd -
+    echo ""
+}
+
+build_cpp
+execute_unit_tests_cpp
+
+sleep 2
+cd cpp/st/
+./st &
+server_pid=$!
+cd -
+sleep 2
+
+
+cd tst
+pytest
 ret=$?
 if [ "$ret" = "0" ]; then
-    echo "CPP UTESTS SUCCESS!!!!"
+    echo "CPP ST FUNCTIONAL SUCCESS!!!!"
 else
-    echo "CPP UTESTS FAILED!!!!!"
+    echo "CPP ST FUNCTIONAL FAILED!!!!!"
     exit 1 
 fi 
 cd -
-echo ""
+
+kill $server_pid
+
+sleep 2
+cd cpp/mt/
+./mt &
+server_pid=$!
+cd -
+sleep 2
+
+
+cd tst
+pytest
+ret=$?
+if [ "$ret" = "0" ]; then
+    echo "CPP MT FUNCTIONAL SUCCESS!!!!"
+else
+    echo "CPP MT FUNCTIONAL FAILED!!!!!"
+    exit 1 
+fi 
+cd -
+
+kill $server_pid
+
+
+
