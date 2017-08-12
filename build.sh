a756ac45952b71dd181a4e191c3524e2fdd29866
orc <lalanne@gmail.com>
2017-08-12 01:59:46 -0400

    #32 ADD: execution of functional tests python

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 echo "Building Project..."
 echo ""
 
+./build_py.sh
 ./build_cpp.sh
 ./build_go.sh
-./build_py.sh
 
 
 
--- build_py.sh
+++ build_py.sh
@@ -22,3 +22,48 @@ else
 fi 
 cd -
 echo ""
+
+
+sleep 2
+cd py/st/
+export PYTHONPATH=../common/
+python server.py &
+server_pid=$!
+cd -
+sleep 2
+
+
+cd tst
+pytest
+ret=$?
+if [ "$ret" = "0" ]; then
+    echo "PY ST FUNCTIONAL SUCCESS!!!!"
+else
+    echo "PY ST FUNCTIONAL FAILED!!!!!"
+    exit 1 
+fi 
+cd -
+
+kill $server_pid
+
+sleep 2
+cd py/mt/
+export PYTHONPATH=../common/
+python server.py &
+server_pid=$!
+cd -
+sleep 2
+
+
+cd tst
+pytest
+ret=$?
+if [ "$ret" = "0" ]; then
+    echo "PY MT FUNCTIONAL SUCCESS!!!!"
+else
+    echo "PY MT FUNCTIONAL FAILED!!!!!"
+    exit 1 
+fi 
+cd -
+
+kill $server_pid
