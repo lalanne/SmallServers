4cd4550a6da6016573bb5c8d0c5146c51b503eec
orc <lalanne@gmail.com>
2017-10-23 23:35:57 -0300

    #50 ADD logging option

--- go/src/st/server.go
+++ go/src/st/server.go
@@ -11,6 +11,7 @@ import (
 const (
 	CONN_TYPE = "tcp"
 	SUCCESS   = 0
+	FAIL      = -1
 )
 
 func handleRequest(connection net.Conn, message []byte) {
@@ -26,13 +27,25 @@ func handleRequest(connection net.Conn, message []byte) {
 func main() {
 
 	fmt.Println("Number of arguments: [" + strconv.Itoa(len(os.Args)) + "]")
-	if len(os.Args) != 3 {
+	if len(os.Args) < 3 && len(os.Args) > 4 {
 		fmt.Println("Error: Wrong number of command line arguments!!!")
 		os.Exit(SUCCESS)
 	}
 
 	ip := os.Args[1]
 	port := os.Args[2]
+	logs := false
+
+	if len(os.Args) == 4 {
+		if os.Args[3] == "on" {
+			logs = true
+		} else if os.Args[3] == "off" {
+			logs = false
+		} else {
+			fmt.Println("Error: wrong option for logging!!!")
+			os.Exit(FAIL)
+		}
+	}
 
 	l, err := net.Listen(CONN_TYPE, ip+":"+port)
 
@@ -57,7 +70,9 @@ func main() {
 			fmt.Println("Error reading:", err.Error())
 		}
 
-		fmt.Printf("incoming buffer: [%s]\n", buf)
+		if logs {
+			fmt.Printf("incoming buffer: [%s]\n", buf)
+		}
 
 		handleRequest(conn, buf)
 	}
